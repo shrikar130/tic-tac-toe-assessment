05f7a4b156fb4f2f15a5b0106f3472ded9abaab2
shrikar130 <125214638+shrikar130@users.noreply.github.com>
2026-09-06 17:46:32 +0530

    updated host and other things

--- tictactoe-assessment/backend/TicTacToe.Api/Program.cs
+++ tictactoe-assessment/backend/TicTacToe.Api/Program.cs
@@ -3,19 +3,28 @@ using TicTacToe.Api.Services;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers().AddJsonOptions(options =>
-    options.JsonSerializerOptions.Converters.Add(new System.Text.Json.Serialization.JsonStringEnumConverter()));
+    options.JsonSerializerOptions.Converters.Add(
+        new System.Text.Json.Serialization.JsonStringEnumConverter()));
+
 builder.Services.AddSingleton<GameService>();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("Angular", policy => policy
-        .WithOrigins("http://localhost:4200")
+        .WithOrigins(
+            "http://localhost:4200",
+            "https://tic-tac-toe-assessment-fr1.onrender.com"
+        )
         .AllowAnyHeader()
         .AllowAnyMethod());
 });
 
 var app = builder.Build();
+
 app.UseCors("Angular");
+
 app.MapControllers();
+
 app.Run();
 
 public partial class Program { }
